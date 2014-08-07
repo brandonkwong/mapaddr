2ecc485d07968e7fbbd4e915558ae69b0655c5b5
brandonkwong <brandonjk@gmail.com>
2014-08-06 23:19:37 -0700

    Fixed location methods and actions for shallow resource nesting

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -12,8 +12,7 @@ class LocationsController < ApplicationController
   end
 
   def edit
-    if current_user && current_user.id == Group.find(params[:group_id]).user_id
-      @group = Group.find(params[:group_id])
+    if current_user && current_user.id == Location.find(params[:id]).group.user_id
       @location = Location.find(params[:id])
       @group_options = current_user.groups.all.sort_by{ |alpha| alpha.name.downcase }.map{ |g| [ g.name, g.id ] }
     else
@@ -22,7 +21,6 @@ class LocationsController < ApplicationController
   end
 
   def update
-    @group = Group.find(params[:group_id])
     @location = Location.find(params[:id])
     if @location.update_attributes(params.require(:location).permit(:name, :address, :description, :group_id))
       redirect_to root_path
@@ -32,7 +30,6 @@ class LocationsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:group_id])
     Location.find(params[:id]).destroy
     redirect_to root_path
   end
