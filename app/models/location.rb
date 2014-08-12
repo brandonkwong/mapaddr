14be5491a59492dbc8c22c419b0c39774fb7f455
brandonkwong <brandonjk@gmail.com>
2014-08-12 11:35:37 -0700

    Setup user view and collapsed groups and locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,6 +3,9 @@ class Location
   field :name, type: String
   field :address, type: String
   field :description, type: String
+  
+  field :latitude, type: String
+  field :longitude, type: String
 
   belongs_to :group
   belongs_to :user
