21d361efb38aeb4bb4a683aee8c28493dbc2e699
brandonkwong <brandonjk@gmail.com>
2014-08-16 15:35:43 -0700

    Initialized map with location markers and centered map to marker on click

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,12 +4,10 @@ mapAddr.controller('MapAddrController', ['$scope', function($scope) {
 
   // Map
   var map;
-  var marker;
 
   // Map Options
   var mapOptions = {
     mapTypeId: google.maps.MapTypeId.ROADMAP,
-    // scrollwheel: false,
     styles: stylized,
     minZoom: 3,
     zoom: 17
@@ -17,9 +15,9 @@ mapAddr.controller('MapAddrController', ['$scope', function($scope) {
 
   // Geolocation
   var initialLocation;
+  var browserSupportFlag = new Boolean();
   var siberia = new google.maps.LatLng(60, 105);
   var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-  var browserSupportFlag = new Boolean();
 
   // Geocoder
   var geocoder;
@@ -32,34 +30,6 @@ mapAddr.controller('MapAddrController', ['$scope', function($scope) {
 
   // Google Maps Initialize
   $scope.mapInit = function() {
-    
-    // Location Latitutde and Longitude
-    // var locations = [
-    //   {
-    //     lat: -25.363882,
-    //     lng: 131.044922,
-    //     title: 'Marker 1'
-    //   },
-
-    //   {
-    //     lat: -50.363882,
-    //     lng: 131.044922,
-    //     title: 'Marker 2'
-    //   },
-
-    //   {
-    //     lat: -33.363882,
-    //     lng: 131.044922,
-    //     title: 'Marker 3'
-    //   },
-
-    //   {
-    //     lat: -44.363882,
-    //     lng: 131.044922,
-    //     title: 'Marker 4'
-    //   }
-    // ];
-
 
     // Map-Canvas Id
     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
@@ -91,7 +61,7 @@ mapAddr.controller('MapAddrController', ['$scope', function($scope) {
           fillOpacity: 0.15,
           map: map,
           center: initialLocation,
-          radius: 200
+          radius: 250
         };
         geoCircle = new google.maps.Circle(circle);
 
@@ -124,16 +94,19 @@ mapAddr.controller('MapAddrController', ['$scope', function($scope) {
 
     // directionsDisplay = new google.maps.DirectionsRenderer();
 
+
+
   }();
 
 
-  // Geocoding Address
-  geocoder = new google.maps.Geocoder();
 
+
+  // Geocoding
+  geocoder = new google.maps.Geocoder();
   $scope.codeAddress = function(address) {
     geocoder.geocode({'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
+        // map.setCenter(results[0].geometry.location);
         var marker = new google.maps.Marker({
             map: map,
             position: results[0].geometry.location
@@ -142,8 +115,21 @@ mapAddr.controller('MapAddrController', ['$scope', function($scope) {
         alert("Geocode was not successful for the following reason: " + status);
       }
     });
-
   };
+
+  // Take Me There
+  $scope.takeMe = function(address) {
+    geocoder.geocode({'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+  };
+
+
+  
 
 
   // // Directions
@@ -161,6 +147,8 @@ mapAddr.controller('MapAddrController', ['$scope', function($scope) {
   //     }
   //   });
   // }
+
+
 
 
 }]);
